6f0f8048ba04f3b0fc0043a7074b727ad5cae733
Youssef Gharbi <joe.gharbi@gmail.com>
2025-04-17 17:03:02 +0200

    Refactor install_benchmarks.sh to allow passing custom directories for Docker builds

--- install_benchmarks.sh
+++ install_benchmarks.sh
@@ -2,13 +2,26 @@
 
 # Install all Docker images for the benchmarks
 
-# Define the directories containing Dockerfiles
-image_dirs=("./containers/static/apache-deb" "./containers/static/nginx-deb" "./containers/static/cowboy-play" "./containers/static/yaws-deb" "./containers/dynamic/nginx-dynamic-deb" "./containers/dynamic/yaws-dynamic-latest-deb")
+# Default directories containing Dockerfiles
+default_image_dirs=("./containers/static/apache-deb" "./containers/static/nginx-deb" "./containers/static/cowboy-play" "./containers/static/yaws-deb" "./containers/dynamic/nginx-dynamic-deb" "./containers/dynamic/yaws-dynamic-latest-deb")
 
-echo "Processing directory: ."
+# Allow additional directories to be passed as arguments
+if [[ $# -gt 0 ]]; then
+    image_dirs=("$@")
+else
+    image_dirs=("${default_image_dirs[@]}")
+fi
+
+echo "Processing directories: ${image_dirs[*]}"
 
 # Loop through each directory and build the Docker image
 for dir in "${image_dirs[@]}"; do
+    # Skip the "brainstorming" folder
+    if [[ "$dir" == *"brainstorming"* ]]; then
+        echo "Skipping directory: $dir"
+        continue
+    fi
+
     dockerfile_path=$(find "$dir" -name "Dockerfile*" -type f)
     if [[ -z "$dockerfile_path" ]]; then
         echo "No Dockerfile found in $dir, skipping."
