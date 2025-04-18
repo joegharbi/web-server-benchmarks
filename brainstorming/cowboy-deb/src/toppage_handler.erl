65d1f29bcb1200109702b08db5f2d922808ea7e9
Youssef Gharbi <joe.gharbi@gmail.com>
2025-04-18 09:36:17 +0200

    Remove ignored files and folders from tracking

--- brainstorming/cowboy-deb/src/toppage_handler.erl
+++
@@ -1,9 +0,0 @@
--module(toppage_handler).
--export([init/2]).
-
-init(Req0, State) ->
-    Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"text/plain">>},
-        <<"Howdy, Cowboy!">>,
-        Req0),
-    {ok, Req, State}.
--- brainstorming/cowboy-test/src/hello_handler.erl
+++
@@ -1,9 +0,0 @@
--module(hello_handler).
--export([init/2]).
-
-init(Req0, State) ->
-    Req = cowboy_req:reply(200,
-        #{<<"content-type">> => <<"text/plain">>},
-        <<"Hello, World!">>,
-        Req0),
-    {ok, Req, State}.
